350da35b2c1afd88ee9edf8400f2364e5983dcda
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-11 22:02:18 +0500

    new pages

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+	
+	$('#custom-width-modal').modal({backdrop: 'static', keyboard: false});
+    $(window).load(function(){
+        $('#custom-width-modal').modal('show');
+    });
+    
+});
