ce7f9a8f39871ef601e69fe4395748e83ed64e82
Shahid Bashir <shahid.chodhry@gmail.com>
2016-01-12 01:44:27 +0500

    new page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,30 @@ jQuery(document).ready(function($) {
     $(window).load(function(){
         $('#custom-width-modal').modal('show');
     });
+
+    $('#timePicker_1').timepicker();
+	$('#timePicker_2').timepicker();
+	$('#timePicker_3').timepicker();
+	$('#timePicker_4').timepicker();
+	$('#timePicker_5').timepicker();
+	$('#timePicker_6').timepicker();
+	$('#timePicker_7').timepicker();
+	$('#timePicker_8').timepicker();
+	$('#timePicker_9').timepicker();
+	$('#timePicker_10').timepicker();
+	$('#timePicker_11').timepicker();
+	$('#timePicker_12').timepicker();
+	$('#timePicker_13').timepicker();
+	$('#timePicker_14').timepicker();
+	$('#timePicker_15').timepicker();
+	$('#timePicker_16').timepicker();
+	$('#timePicker_17').timepicker();
+	$('#timePicker_18').timepicker();
+	$('#timePicker_19').timepicker();
+	$('#timePicker_20').timepicker();
+	$('#timePicker_21').timepicker();
+	$('#timePicker_22').timepicker();
+
+	$('#datetimepicker').datetimepicker();
     
 });
